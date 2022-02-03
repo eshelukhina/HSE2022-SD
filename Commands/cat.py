3aff68df5921e2f8c6ebc119241649ba4a4a13b3
Екатерина Шелухина <shelukchina2447@gmail.com>
2022-02-03 21:47:00 +0300

    Add cat

--- Commands/cat.py
+++ Commands/cat.py
@@ -1,12 +1,23 @@
 from typing import List
 
+from Executor.file_manager import FileManager
+from Executor.path_types import PathTypes
+
 
 class Cat:
     def __init__(self, args: List[str]):
         self.args = args
 
     def execute(self):
-        pass
+        for arg in self.args:
+            file_manager = FileManager(arg)
+            file_manager.get_file_type()
+            if file_manager.file_type == PathTypes.file:
+                print(file_manager.get_file_content())
+            elif file_manager.file_type == PathTypes.directory:
+                print('cat: Is a directory')
+            else:
+                print('cat: No such file or directory')
 
     def __eq__(self, other):
         if isinstance(other, Cat):
