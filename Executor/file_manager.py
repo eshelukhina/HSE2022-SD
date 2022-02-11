ac7ed5090b921102443b2d6be66c5f7fa4cf7962
egiptipavel <kinqstone585@gmail.com>
2022-02-11 19:55:19 +0300

    Add description for file manager methods

--- Executor/file_manager.py
+++ Executor/file_manager.py
@@ -9,21 +9,33 @@ class FileManager:
 
     @staticmethod
     def is_file(path_to_file):
+        """
+        Check if file exists
+        :rtype bool
+        """
         return path.isfile(path_to_file)
 
     @staticmethod
-    def is_directory(path_to_directory):
-        return path.isdir(path_to_directory)
-
-    @staticmethod
     def get_file_content(path_to_file):
+        """
+        Returns the file content
+        :rtype str
+        """
         with open(path_to_file, "r") as f:
             return str(f.read())
 
     @staticmethod
     def get_directory_content(path_to_directory):
+        """
+        Returns the names of files in a directory
+        :rtype list[str]
+        """
         return [os.path.join(path_to_directory, x) for x in os.listdir(path_to_directory)]
 
     @staticmethod
     def get_current_directory():
+        """
+        Returns the path to the working directory
+        :rtype str
+        """
         return os.getcwd()
