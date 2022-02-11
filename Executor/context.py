b443c8c91dd5d838c161b16bcb51fd09491a9164
egiptipavel <kinqstone585@gmail.com>
2022-02-11 15:18:58 +0300

    Add description of Context

--- Executor/context.py
+++ Executor/context.py
@@ -1,11 +1,16 @@
 from optional import Optional
 
+from Environment.impl import Environment
+
 
 class Context:
-    """
-
-    """
-    def __init__(self, *, env_vars):
+    def __init__(self, *, env_vars: Environment):
+        """
+        Сlass that stores the execution context of the commands.
+        :param error: command execution error message
+        :param state: result of the previous command
+        :param env_vars: enviroment
+        """
         self.error = Optional.empty()
         self.state = Optional.empty()
         self.env = env_vars
