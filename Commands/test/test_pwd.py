3a13fcd85cc8d525e2df276f321320d9bc63c4e7
achaikova <57727520+achaikova@users.noreply.github.com>
2022-02-14 02:46:32 +0300

    add pwd test

--- Commands/test/test_pwd.py
+++ Commands/test/test_pwd.py
@@ -12,3 +12,13 @@ def test_cur_dir():
     ret_code = pwd.execute(context)
     assert ret_code == 0
     assert context.state.get() == result
+
+
+def test_non_empty_dir():
+    args = ['hello world!']
+    err_result = "pwd: too many arguments"
+    pwd = Pwd(args=args)
+    context = Context()
+    ret_code = pwd.execute(context)
+    assert ret_code == 1
+    assert context.state.get() == err_result
