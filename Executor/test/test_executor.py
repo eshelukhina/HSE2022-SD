cebbabe1802c987261a2eb2ad047b685be1c2e1d
achaikova <57727520+achaikova@users.noreply.github.com>
2022-02-14 04:24:03 +0300

    add pipe tests

--- Executor/test/test_executor.py
+++ Executor/test/test_executor.py
@@ -1,5 +1,7 @@
+from Commands.cat import Cat
 from Commands.echo import Echo
 from Commands.exit import Exit
+from Commands.pwd import Pwd
 from Executor.executor import Executor
 
 
@@ -29,3 +31,39 @@ def test_exit():
     assert ret_code == 0
     assert output.get() == command.output
     assert executor.is_shell_terminated
+
+
+def test_several_commands():
+    result = 'hello world !'
+    command1 = Echo(args=['hello', 'world', '!'])
+    command2 = Cat(args=[])
+    executor = Executor()
+    executor.set_commands([command1, command2])
+    output, ret_code = executor.run()
+    assert ret_code == 0
+    assert output.get() == result
+
+
+def test_several_commands_exit():
+    result = ''
+    command1 = Echo(args=['hello', 'world', '!'])
+    command2 = Exit(args=[])
+    command3 = Cat(args=[])
+    executor = Executor()
+    executor.set_commands([command1, command2, command3])
+    output, ret_code = executor.run()
+    assert ret_code == 0
+    assert output.get() == result
+    assert executor.is_shell_terminated
+
+
+def test_pipe_err_output():
+    result = 'pwd: too many arguments'
+    command1 = Echo(args=['hello', 'world', '!'])
+    command2 = Pwd(args=[])
+    command3 = Cat(args=[])
+    executor = Executor()
+    executor.set_commands([command1, command2, command3])
+    output, ret_code = executor.run()
+    assert ret_code == 1
+    assert output.get() == result
