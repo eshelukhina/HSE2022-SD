eb90944a220701eb114cdc88836d54dc0faabfc6
Daniel Gabitov <work.gabitov@gmail.com>
2022-02-18 22:57:29 +0300

    Katya is the best

--- Commands/grep.py
+++ Commands/grep.py
@@ -1,7 +1,9 @@
 import argparse
-from typing import List, Tuple
+import re
+from typing import List, Tuple, Optional
 
 from Executor.context import Context
+from Executor.file_manager import FileManager
 
 
 class ArgumentParserError(Exception):
@@ -11,6 +13,23 @@ class ArgumentParserError(Exception):
 class ThrowingArgumentParser(argparse.ArgumentParser):
     def error(self, message):
         raise ArgumentParserError(message)
+
+
+def search(*, data: str, pattern: str, ignore_case, file_name: Optional[str], append_number: int):
+    s = set()
+    split_data = data.split('\n')
+    for i, line in enumerate(split_data):
+        if re.search(pattern, line, ignore_case):
+            s.add(i)
+            s = s.union(set(range(i + 1, min(i + 1 + append_number, len(split_data)))))
+    result = []
+    for line_no in s:
+        line = ''
+        if file_name:
+            line += f'{file_name}: '
+        line += split_data[line_no]
+        result.append(line)
+    return result
 
 
 class Grep:
@@ -25,13 +44,12 @@ class Grep:
         self.args = args
 
     def parse_arguments(self, args):
-        parser = ThrowingArgumentParser(exit_on_error=False)
+        parser = ThrowingArgumentParser()
         parser.add_argument("-w", dest="words", action="store_true", default=False)
         parser.add_argument("-i", dest="case_insensitive", action="store_true", default=False)
-        parser.add_argument("-A", dest="count", type=int)
+        parser.add_argument("-A", dest="count", type=int, default=0)
         parser.add_argument("pattern")
-        parser.add_argument("files", nargs="+")
-        return parser.parse_args(args)
+        return parser.parse_known_args(args)
 
     def execute(self, context: Context) -> Tuple[str, int]:
         """
@@ -39,9 +57,35 @@ class Grep:
         :returns: Status code
         :rtype: int
         """
-        result_of_parsing = self.parse_arguments(self.args)
-        # TODO написать реализацию
-        return "", 0
+        try:
+            result_of_parsing, files = self.parse_arguments(self.args)
+        except (ArgumentParserError, ThrowingArgumentParser, argparse.ArgumentError) as e:
+            return e.__str__(), 1
+        ignore_case = 0
+        pattern = result_of_parsing.pattern
+        if result_of_parsing.case_insensitive:
+            ignore_case |= re.IGNORECASE
+        if result_of_parsing.words:
+            pattern = '([^A-Za-z0-9_]' + pattern + '[^A-Za-z0-9_]|' + \
+                      '^' + pattern + '[^A-Za-z0-9_]|' +\
+                      '^' + pattern + '$|' +\
+                      '[^A-Za-z0-9_]' + pattern + '$)'
+        result = []
+        if not files:
+            if not context.state:
+                return "grep: empty input", 1
+            result += search(data=context.state.get(), pattern=pattern,
+                             ignore_case=ignore_case, file_name=None,
+                             append_number=result_of_parsing.count)
+        else:
+            for file_name in files:
+                if not FileManager.is_file(file_name):
+                    return f'grep: no such file {file_name}', 2
+                file_content = FileManager.get_file_content(file_name)
+                result += search(data=file_content, pattern=pattern,
+                                 ignore_case=ignore_case, file_name=file_name,
+                                 append_number=result_of_parsing.count)
+        return '\n'.join(result), 0
 
     def __str__(self):
         return f'GREP {self.args}'
--- Commands/test/test_grep.py
+++ Commands/test/test_grep.py
@@ -1,86 +1,247 @@
 import argparse
+import os
+import pytest
+import tempfile
 from argparse import Namespace
 
-import pytest
-
 from Commands.grep import Grep, ArgumentParserError
+from Executor.context import Context
+
+
+def create_tmp_file(content):
+    fd, path = tempfile.mkstemp()
+    with os.fdopen(fd, 'w') as tmp:
+        tmp.write(content)
+    return fd, path
 
 
 def test_parse_arguments_without_files():
     args = ["str"]
     grep = Grep(args=args)
-    with pytest.raises(ArgumentParserError):
-        grep.parse_arguments(args)
+    result = Namespace(pattern="str", count=0, case_insensitive=False, words=False)
+    output, files = grep.parse_arguments(args)
+    assert output == result
+    assert files == []
 
 
 def test_parse_arguments_with_one_file_and_no_flags():
     args = ["str", "file.txt"]
-    result = Namespace(pattern="str", files=["file.txt"], count=None, case_insensitive=False, words=False)
+    result = Namespace(pattern="str", count=0, case_insensitive=False, words=False)
     grep = Grep(args=args)
-    output = grep.parse_arguments(args)
+    output, files = grep.parse_arguments(args)
     assert output == result
+    assert files == ["file.txt"]
 
 
 def test_parse_arguments_with_files_and_no_flags():
     args = ["str", "file.txt", "file2.txt", "file3.txt"]
-    result = Namespace(pattern="str", files=["file.txt", "file2.txt", "file3.txt"], count=None, case_insensitive=False,
+    result = Namespace(pattern="str", count=0, case_insensitive=False,
                        words=False)
     grep = Grep(args=args)
-    output = grep.parse_arguments(args)
+    output, files = grep.parse_arguments(args)
     assert output == result
+    assert files == ["file.txt", "file2.txt", "file3.txt"]
 
 
 def test_parse_arguments_with_one_file_and_words_flag():
     args = ["-w", "str", "file.txt"]
-    result = Namespace(pattern="str", files=["file.txt"], count=None, case_insensitive=False, words=True)
+    result = Namespace(pattern="str", count=0, case_insensitive=False, words=True)
     grep = Grep(args=args)
-    output = grep.parse_arguments(args)
+    output, files = grep.parse_arguments(args)
     assert output == result
+    assert files == ["file.txt"]
     args = ["str", "-w", "file.txt"]
-    output = grep.parse_arguments(args)
+    output, files = grep.parse_arguments(args)
     assert output == result
+    assert files == ["file.txt"]
     args = ["str", "file.txt", "-w"]
-    output = grep.parse_arguments(args)
+    output, files = grep.parse_arguments(args)
     assert output == result
+    assert files == ["file.txt"]
 
 
 def test_parse_arguments_with_one_file_and_case_insensitive_flag():
     args = ["-i", "str", "file.txt"]
-    result = Namespace(pattern="str", files=["file.txt"], count=None, case_insensitive=True, words=False)
+    result = Namespace(pattern="str", count=0, case_insensitive=True, words=False)
     grep = Grep(args=args)
-    output = grep.parse_arguments(args)
+    output, files = grep.parse_arguments(args)
     assert output == result
+    assert files == ["file.txt"]
     args = ["str", "-i", "file.txt"]
-    output = grep.parse_arguments(args)
+    output, files = grep.parse_arguments(args)
     assert output == result
+    assert files == ["file.txt"]
     args = ["str", "file.txt", "-i"]
-    output = grep.parse_arguments(args)
+    output, files = grep.parse_arguments(args)
     assert output == result
+    assert files == ["file.txt"]
 
 
 def test_parse_arguments_with_one_file_and_count_flag():
     args = ["-A", "str", "file.txt"]
     grep = Grep(args=args)
-    with pytest.raises(argparse.ArgumentError):
+    with pytest.raises(ArgumentParserError):
         grep.parse_arguments(args)
     args = ["-A", "0", "str", "file.txt"]
-    result = Namespace(pattern="str", files=["file.txt"], count=0, case_insensitive=False, words=False)
-    output = grep.parse_arguments(args)
+    result = Namespace(pattern="str", count=0, case_insensitive=False, words=False)
+    output, files = grep.parse_arguments(args)
     assert output == result
+    assert files == ["file.txt"]
     args = ["str", "file.txt", "-A", "0"]
-    output = grep.parse_arguments(args)
+    output, files = grep.parse_arguments(args)
     assert output == result
+    assert files == ["file.txt"]
     args = ["str", "-A", "0", "file.txt"]
-    output = grep.parse_arguments(args)
+    output, files = grep.parse_arguments(args)
     assert output == result
+    assert files == ["file.txt"]
     args = ["str", "file.txt", "-A", "0"]
-    output = grep.parse_arguments(args)
+    output, files = grep.parse_arguments(args)
     assert output == result
+    assert files == ["file.txt"]
 
 
 def test_parse_arguments_with_all_flags():
     args = ["-A", "0", "-w", "-i", "str", "file.txt"]
     grep = Grep(args=args)
-    result = Namespace(pattern="str", files=["file.txt"], count=0, case_insensitive=True, words=True)
-    output = grep.parse_arguments(args)
+    result = Namespace(pattern="str", count=0, case_insensitive=True, words=True)
+    output, files = grep.parse_arguments(args)
     assert output == result
+    assert files == ["file.txt"]
+
+
+def test_simple_string_execute():
+    content = 'hello\nworld'
+    context = Context()
+    fd1, path1 = create_tmp_file(content)
+
+    args = ["hello", path1]
+    grep = Grep(args=args)
+    output, ret_code = grep.execute(context)
+
+    assert ret_code == 0
+    assert output == f'{path1}: hello'
+
+
+def test_or_string_execute():
+    content = 'hello world\nbye\nhello bye\ngray'
+    context = Context()
+    fd1, path1 = create_tmp_file(content)
+
+    args = ["hello|bye", path1]
+    grep = Grep(args=args)
+    output, ret_code = grep.execute(context)
+
+    assert ret_code == 0
+    assert output == f'{path1}: hello world\n{path1}: bye\n{path1}: hello bye'
+
+
+def test_or_inside_string_execute():
+    content = 'gray, grey\ngrey 1233\ngray 1244\nhello world\nend grey'
+    context = Context()
+    fd1, path1 = create_tmp_file(content)
+
+    args = ["gr(a|e)y", path1]
+    grep = Grep(args=args)
+    output, ret_code = grep.execute(context)
+
+    assert ret_code == 0
+    assert output == f'{path1}: gray, grey\n{path1}: grey 1233\n{path1}: gray 1244\n{path1}: end grey'
+
+
+def test_char_array_string_execute():
+    content = "babble\nbebble\nbibble\nbobble\nbubble\nhello world\n babble hello\nno such grep"
+    context = Context()
+    fd1, path1 = create_tmp_file(content)
+
+    args = ["b[aeiou]bble", path1]
+    grep = Grep(args=args)
+    output, ret_code = grep.execute(context)
+
+    assert ret_code == 0
+    assert output == (f"{path1}: babble\n{path1}: bebble\n{path1}: bibble\n"
+                      f"{path1}: bobble\n{path1}: bubble\n{path1}:  babble hello")
+
+
+def test_simple_string_multiple_files():
+    content1 = "hello world1\nhello1\nworld\nbye"
+    content2 = "world\nbye\nhello world2\nhello2"
+    context = Context()
+
+    fd1, path1 = create_tmp_file(content1)
+    fd2, path2 = create_tmp_file(content2)
+
+    args = ["hello", path1, path2]
+    grep = Grep(args=args)
+    output, ret_code = grep.execute(context)
+
+    assert ret_code == 0
+    assert output == f"{path1}: hello world1\n{path1}: hello1\n" + f"{path2}: hello world2\n{path2}: hello2"
+
+
+def test_ignore_case():
+    content = "Hello IM PASHA\nhello IM KATYA"
+    context = Context()
+
+    fd, path = create_tmp_file(content)
+
+    args = ["Hello", path, '-i']
+    grep = Grep(args=args)
+    output, ret_code = grep.execute(context)
+
+    assert ret_code == 0
+    assert output == f'{path}: Hello IM PASHA\n{path}: hello IM KATYA'
+
+
+def test_ignore_case_two_files():
+    content1 = "Hello World\n hello world"
+    content2 = "Hello World1\n hello world"
+
+    context = Context()
+    fd1, path1 = create_tmp_file(content1)
+    fd2, path2 = create_tmp_file(content2)
+
+    args = ["hello", "-i", path1, path2]
+    grep = Grep(args=args)
+    output, ret_code = grep.execute(context)
+    print(output)
+    assert ret_code == 0
+    assert output == f"{path1}: Hello World\n{path1}:  hello world\n" + f"{path2}: Hello World1\n{path2}:  hello world"
+
+
+def test_search_like_word():
+    content = "hello worldmy name id daniel\nhello world"
+    context = Context()
+    fd, path = create_tmp_file(content)
+    args = ["hello world", "-w", path]
+    grep = Grep(args=args)
+    output, ret_code = grep.execute(context)
+    print(output)
+    assert ret_code == 0
+    assert output == f"{path}: hello world"
+
+
+def test_use_flag_a():
+    content = "hello\nworld1\nworld2\nworld3\nworld4\nworld5"
+    context = Context()
+    fd, path = create_tmp_file(content)
+    args = ["hello", "-A", "5", path]
+
+    grep = Grep(args=args)
+    output, ret_code = grep.execute(context)
+    print(output)
+    assert ret_code == 0
+    assert output == f"{path}: hello\n{path}: world1\n{path}: world2\n{path}: world3\n{path}: world4\n{path}: world5"
+
+
+def test_use_flag_a_double_find():
+    content = "hello\nhello\nworld2\nworld3\nworld4\nworld5"
+    context = Context()
+    fd, path = create_tmp_file(content)
+    args = ["hello", "-A", "3", path]
+
+    grep = Grep(args=args)
+    output, ret_code = grep.execute(context)
+
+    assert ret_code == 0
+    assert output == f"{path}: hello\n{path}: hello\n{path}: world2\n{path}: world3\n{path}: world4"
